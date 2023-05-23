7372e77064f7b4011f81583d8facc688543a467b
parisdesign <paris_raz@yahoo.com>
2023-05-23 10:30:17 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Give You Glory"],
+      body: ["Gochi Hand"],
+    },
+  },
+  plugins: [],
+};
